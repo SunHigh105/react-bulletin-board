606bffef6a4a62d1ad8d6deda524da767514d7ba
Ayaka Hidaka <hidaka.ayaka105@gmail.com>
2021-07-07 21:49:35 +0900

    テーブル追加

--- database/migrations/2021_07_05_231925_create_videos_table.php
+++ database/migrations/2021_07_05_231925_create_videos_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateFlightsTable extends Migration
+class CreateVideosTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,10 +13,11 @@ class CreateFlightsTable extends Migration
      */
     public function up()
     {
-        Schema::create('flights', function (Blueprint $table) {
+        Schema::create('videos', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('airline');
+            $table->integer('playlist_id');
+            $table->string('url');
             $table->timestamps();
         });
     }
@@ -28,6 +29,6 @@ class CreateFlightsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('flights');
+        Schema::dropIfExists('videos');
     }
 }
--- database/migrations/2021_07_05_232007_create_video_playlists_table.php
+++ database/migrations/2021_07_05_232007_create_video_playlists_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateFlightsTable extends Migration
+class CreateVideoPlaylistsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,10 +13,11 @@ class CreateFlightsTable extends Migration
      */
     public function up()
     {
-        Schema::create('flights', function (Blueprint $table) {
+        Schema::create('video_playlists', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('airline');
+            $table->integer('user_id');
+            $table->boolean('is_public');
             $table->timestamps();
         });
     }
@@ -28,6 +29,6 @@ class CreateFlightsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('flights');
+        Schema::dropIfExists('video_playlists');
     }
 }
